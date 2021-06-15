d95f3882abc2fa599b8dec4cf9f384a034e2cb68
Sebastian San Blas <sebastiansanblas@gmail.com>
2021-06-15 07:17:48 -0300

    Styles and fixes

--- BMI Calculator/ViewController.swift
+++ BMI Calculator/ViewController.swift
@@ -33,6 +33,7 @@ class ViewController: UIViewController, UITextFieldDelegate {
         self.heightField.inputAccessoryView = toolbar
         self.weightField.inputAccessoryView = toolbar
         outputBMIField.text = ""
+        outputBMIField.backgroundColor = UIColor(red: 0/255, green: 185/255, blue: 26/255, alpha: 1.0)
     }
 
 
@@ -48,32 +49,41 @@ class ViewController: UIViewController, UITextFieldDelegate {
         outputBMIField.text = String(format: "%.2f", result)
         switch result {
         case ..<16:
+            // Rojo R 216, G 36, B 7
             print("Severe Thinness")
-            outputBMIField.backgroundColor = UIColor(red: 0/255, green: 159/255, blue: 184/255, alpha: 1.0)
+            outputBMIField.backgroundColor = UIColor(red: 216/255, green: 36/255, blue: 7/255, alpha: 1.0)
         case 16..<17:
+            //Naranja R 234, G 117, B 19
             print("Moderate Thinness")
-            outputBMIField.backgroundColor = UIColor(red: 0/255, green: 159/255, blue: 184/255, alpha: 1.0)
+            outputBMIField.backgroundColor = UIColor(red: 234/255, green: 117/255, blue: 19/255, alpha: 1.0)
         case 17..<18.5:
+            //Amarillo R 229, G 203, B 14
             print("Mild Thinness ")
-            outputBMIField.backgroundColor = UIColor(red: 0/255, green: 159/255, blue: 184/255, alpha: 1.0)
+            outputBMIField.backgroundColor = UIColor(red: 229/255, green: 203/255, blue: 14/255, alpha: 1.0)
         case 18.5..<25:
+            //Verde R 0, G 185, B 26
             print("Normal")
-            outputBMIField.backgroundColor = UIColor(red: 0/255, green: 159/255, blue: 184/255, alpha: 1.0)
+            outputBMIField.backgroundColor = UIColor(red: 0/255, green: 185/255, blue: 26/255, alpha: 1.0)
         case 25..<30:
+            //Amarillo R 229, G 203, B 14
             print("Overweight")
-            outputBMIField.backgroundColor = UIColor(red: 0/255, green: 159/255, blue: 184/255, alpha: 1.0)
+            outputBMIField.backgroundColor = UIColor(red: 229/255, green: 203/255, blue: 14/255, alpha: 1.0)
         case 30..<35:
+            //Naranja R 234, G 117, B 19
             print("Obese Class I")
-            outputBMIField.backgroundColor = UIColor(red: 0/255, green: 159/255, blue: 184/255, alpha: 1.0)
+            outputBMIField.backgroundColor = UIColor(red: 234/255, green: 117/255, blue: 19/255, alpha: 1.0)
         case 35..<40:
+            //Rojo R 216, G 36, B 7
             print("Obese Class II")
-            outputBMIField.backgroundColor = UIColor(red: 0/255, green: 159/255, blue: 184/255, alpha: 1.0)
+            outputBMIField.backgroundColor = UIColor(red: 216/255, green: 36/255, blue: 7/255, alpha: 1.0)
         case 40...:
+            //Bordó R 150, G 12, B 12
             print("Obese Class III")
-            outputBMIField.backgroundColor = UIColor(red: 0/255, green: 159/255, blue: 184/255, alpha: 1.0)
+            outputBMIField.backgroundColor = UIColor(red: 150/255, green: 12/255, blue: 12/255, alpha: 1.0)
         default:
             fatalError()
         }
+        dismissMyKeyboard()
     }
     
     @objc func dismissMyKeyboard() {
